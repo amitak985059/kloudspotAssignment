ac311a219a5ca675b00c255b879d82704ee49e99
Amit <amitak985059@gmail.com>
2025-12-17 19:31:14 +0530

    ui

--- client/src/components/dashboard/MetricCard.jsx
+++ client/src/components/dashboard/MetricCard.jsx
@@ -32,13 +32,9 @@ const MetricCard = ({ title, value, change, loading }) => {
                 }`}
               >
                 {isPositive ? (
-                  <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                  </svg>
+                  <img src="/up.svg" alt="" />
                 ) : (
-                  <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                  </svg>
+                  <img src="/down.svg" alt="" />
                 )}
                 {Math.abs(change)}%
               </span>
